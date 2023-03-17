79e23d67e23a5c1acd0ecdf7c69a095eca49e945
David Anekstein <da@immunant.com>
2023-03-17 14:58:21 -0400

    add CLI for vivisect server

--- plugins/vivisect-broker/broker/vivisect_server.py
+++ plugins/vivisect-broker/broker/vivisect_server.py
@@ -5,64 +5,83 @@ from concurrent import futures
 import vivisect
 import Elf
 import grpc
-# binary_path = "/home/davidanekstein/amp/Challenge-Problems-master/Challenge_03/build/program_c"
-binary_path = "/home/davidanekstein/amp/main"
-
-vw = vivisect.VivWorkspace()
-# vw.setMeta('Architecture', 'arm')
-vw.setMeta('bigend', False)
-vw.loadFromFile(binary_path)
-vw.analyze()
-
-elf = Elf.elfFromFileName(binary_path)
+import click
 
 
-emu = vw.getEmulator()
-print(list(map(lambda x: vw.getName(x), vw.getEntryPoints())))
-emu.setProgramCounter(vw.getEntryPoints()[0])
-print(hex(elf.getBaseAddress()))
+class Service(vivserver_pb2_grpc.EmulatorServicer):
+    def __init__(self, binary_path, architecture, endianness) -> None:
+        # binary_path = "/home/davidanekstein/amp/Challenge-Problems-master/Challenge_03/build/program_c"
+        self.binary_path = binary_path
 
+        vw = vivisect.VivWorkspace()
 
-class Greeter(vivserver_pb2_grpc.EmulatorServicer):
+        if architecture:
+            vw.setMeta("Architecture", architecture)
+
+        if endianness == "big":
+            vw.setMeta("bigend", True)
+        if endianness == "little":
+            vw.setMeta("bigend", False)
+
+        vw.loadFromFile(binary_path)
+        vw.analyze()
+
+        elf = Elf.elfFromFileName(binary_path)
+        print(hex(elf.getBaseAddress()))
+
+        self.emu = vw.getEmulator()
+        print(list(map(lambda x: vw.getName(x), vw.getEntryPoints())))
+        self.emu.setProgramCounter(vw.getEntryPoints()[0])
 
     def RunNumInstructions(self, request, context):
         ops = list(self.step(request.numInstructions))
         instructions = []
-        print(ops)
-        for (instruction, _, opCode) in ops:
-            print(instruction, opCode)
-            print(instruction.to_bytes(length=8, byteorder='big'), opCode.to_bytes(length=8, byteorder='big'))
-            print(hex(instruction), hex(opCode))
-            instructions.append(vivserver_pb2.RunInstructionsReply.Instruction(
-                instruction=instruction.to_bytes(length=8, byteorder='big'), opCode=opCode.to_bytes(length=8, byteorder='big')))
+        for instruction, _, opCode in ops:
+            print(f"instruction: {hex(instruction)}, opcode: {hex(opCode)}")
+
+            # although the ISA may be little endian, we don't want to alter the byte order of the int that
+            # vivisect returns before sending it. Big endian maintains the byte order, so that's what we
+            # send it as, but this does not reflect the endianess of the instructions themselves
+            instructions.append(
+                vivserver_pb2.RunInstructionsReply.Instruction(
+                    instruction=instruction.to_bytes(length=8, byteorder="big"),
+                    opCode=opCode.to_bytes(length=8, byteorder="big"),
+                )
+            )
         return vivserver_pb2.RunInstructionsReply(instructions=instructions)
 
     def step(self, n):
         print(n)
         for i in range(1, n):
             try:
-                pc_before = emu.getProgramCounter()
-                op = emu.parseOpcode(emu.getProgramCounter())
-                emu.stepi()
-                instruction = emu.readMemValue(
-                    emu.getProgramCounter(), 4)
+                pc_before = self.emu.getProgramCounter()
+                op = self.emu.parseOpcode(self.emu.getProgramCounter())
+                self.emu.stepi()
+                instruction = self.emu.readMemValue(self.emu.getProgramCounter(), 4)
                 opCode = op.opcode
                 yield instruction, op, opCode
+
             except Exception as e:
                 print(e)
 
 
-def serve():
-    uds_addresses = ['[::]:50051']
+@click.command()
+@click.argument("binary_path")
+@click.option("--architecture", type=click.Choice(["amd64", "arm"]))
+@click.option("--endianness", type=click.Choice(["big", "little"]))
+def serve(binary_path, architecture, endianness):
+    uds_addresses = ["[::]:50051"]
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    vivserver_pb2_grpc.add_EmulatorServicer_to_server(Greeter(), server)
+    vivserver_pb2_grpc.add_EmulatorServicer_to_server(
+        Service(binary_path, architecture, endianness), server
+    )
     for uds_address in uds_addresses:
         server.add_insecure_port(uds_address)
-        logging.info('Server listening on: %s', uds_address)
+        logging.info("Server listening on: %s", uds_address)
     server.start()
     server.wait_for_termination()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     serve()
