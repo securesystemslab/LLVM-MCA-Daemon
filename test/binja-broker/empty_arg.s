fca57737d7deb16267257c917bae734c64527bf6
Chinmay Deshpande <cddeshpa@uci.edu>
2024-07-21 19:29:54 -0700

    Update some tests

--- test/binja-broker/empty_arg.s
+++ test/binja-broker/empty_arg.s
@@ -1,4 +1,4 @@
-# RUN: export MCAD_PORT=$(shuf -i 5000-6000 -n 1)
+# RUN: export MCAD_PORT=50055
 # RUN: llvm-mcad -mtriple=x86_64-unknown-unknown -mcpu=znver2 -load-broker-plugin=$MCAD_BIN_PATH/plugins/binja-broker/libMCADBinjaBroker.so --broker-plugin-arg-host=0.0.0.0:$MCAD_PORT | python3 %S/empty_arg.py --port $MCAD_PORT > %t
 # RUN: FileCheck --input-file=%t %s
 
--- test/binja-broker/invalid_arg.s
+++ test/binja-broker/invalid_arg.s
@@ -1,6 +1,6 @@
-# RUN: export MCAD_PORT=$(shuf -i 5000-6000 -n 1)
+# RUN: export MCAD_PORT=50053
 # RUN: llvm-mcad -mtriple=x86_64-unknown-unknown -mcpu=znver2 -load-broker-plugin=$MCAD_BIN_PATH/plugins/binja-broker/libMCADBinjaBroker.so --broker-plugin-arg-host=0.0.0.0:$MCAD_PORT | python3 %S/invalid_arg.py --port $MCAD_PORT > %t 
 # RUN: FileCheck --input-file=%t %s
 
 # CHECK:      PC Error. Code: StatusCode.INVALID_ARGUMENT
-# CHECK-NEXT: Details: Errors in instruction(s) 0.
+# CHECK-NEXT: Details: Errors in instruction(s) 0 (Disassembler reported Fail).
