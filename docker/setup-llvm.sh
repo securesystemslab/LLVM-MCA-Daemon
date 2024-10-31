31292d2b1f9af63949755ddd716ff183ec590708
andreroesti <an.roesti@gmail.com>
2024-10-31 14:51:52 -0700

    give workflow a git identity

--- docker/setup-llvm.sh
+++ docker/setup-llvm.sh
@@ -10,6 +10,10 @@ fi
 
 # Run ./setup-deps.sh before this to install dependencies needed to build LLVM
 
+# Applying the patches requires that our git user has an identity.
+git config --global user.email "workflow@example.com"
+git config --global user.name "Workflow"
+
 git clone https://github.com/llvm/llvm-project.git llvm
 cd llvm
 git am < ${WORKSPACE_PATH}/patches/add-identifier-to-mca-instruction.patch
