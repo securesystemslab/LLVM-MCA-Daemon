3a97fb8820db13b2cc7dd787211d05289f3a9771
andreroesti <an.roesti@gmail.com>
2024-11-04 15:32:37 -0800

    rework CI and Docker image

--- docker/setup-build.sh
+++ docker/setup-build.sh
@@ -5,15 +5,16 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 
 if [ -z "${WORKSPACE_PATH}" ]; then
-WORKSPACE_PATH=/work/LLVM-MCA-Daemon
+WORKSPACE_PATH=/work
 fi
 
-mkdir -p ${WORKSPACE_PATH}/build
-cd ${WORKSPACE_PATH}/build
+mkdir -p ${WORKSPACE_PATH}/mcad-build
+mkdir -p ${WORKSPACE_PATH}/mcad-install
+cd ${WORKSPACE_PATH}/mcad-build
 
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/opt/LLVM-MCA-Daemon \
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=${WORKSPACE_PATH}/mcad-install \
                -DCMAKE_C_COMPILER=clang-14 -DCMAKE_CXX_COMPILER=clang++-14 \
-               -DLLVM_DIR=/opt/llvm-main/lib/cmake/llvm \
+               -DLLVM_DIR=${WORKSPACE_PATH}/llvm-install/lib/cmake/llvm \
                -DLLVM_MCAD_ENABLE_PLUGINS="tracer;binja;vivisect" \
-               ../
+               ${WORKSPACE_PATH}/LLVM-MCA-Daemon
 ninja
--- docker/setup-llvm.sh
+++ docker/setup-llvm.sh
@@ -5,7 +5,7 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 
 if [ -z "${WORKSPACE_PATH}" ]; then
-WORKSPACE_PATH=/work/LLVM-MCA-Daemon
+WORKSPACE_PATH=/work
 fi
 
 # Run ./setup-deps.sh before this to install dependencies needed to build LLVM
@@ -13,14 +13,11 @@ fi
 # Applying the patches requires that our git user has an identity.
 git config --global user.email "workflow@example.com"
 git config --global user.name "Workflow"
-
 git clone https://github.com/llvm/llvm-project.git llvm
 cd llvm
-git am < ${WORKSPACE_PATH}/patches/add-identifier-to-mca-instruction.patch
-git am < ${WORKSPACE_PATH}/patches/start-mapping-e500-itenerary-model-to-new-schedule.patch
-git am < ${WORKSPACE_PATH}/patches/abstract-memory-group.patch
+git am ${WORKSPACE_PATH}/LLVM-MCA-Daemon/patches/*.patch
 mkdir build && cd build
-cmake -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=/opt/llvm-main \
+cmake -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=${WORKSPACE_PATH}/llvm-install \
                -DCMAKE_C_COMPILER=clang-14 -DCMAKE_CXX_COMPILER=clang++-14 \
                -DLLVM_USE_LINKER=lld-14 -DLLVM_ENABLE_ASSERTIONS=ON \
                -DLLVM_TOOL_LLVM_MCA_BUILD=ON \
