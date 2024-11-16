ca3aa719e7322ac1b7cb6d04cdcd999eb652b95e
andreroesti <an.roesti@gmail.com>
2024-11-15 17:24:14 -0800

    add FetchDelay stage

---
+++ CustomStages/MCADFetchDelayStage.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include "CustomStages/MCADFetchDelayStage.h"
+
+namespace llvm {
+namespace mcad {
+
+struct MCADInstructionFetchedEvent {};
+
+bool MCADFetchDelayStage::hasWorkToComplete() const {
+    return !instrQueue.empty();
+}
+
+bool MCADFetchDelayStage::isAvailable(const llvm::mca::InstRef &IR) const {
+    return checkNextStage(IR);
+}
+
+llvm::Error MCADFetchDelayStage::forwardDueInstrs() {
+    while(!instrQueue.empty() && instrQueue.front().delayCyclesLeft == 0) {
+        llvm::mca::InstRef IR = instrQueue.front().IR;
+        if (llvm::Error Val = moveToTheNextStage(IR)) {
+            return Val;
+        }
+        instrQueue.pop_front();
+    }
+    return llvm::ErrorSuccess();
+}
+
+llvm::Error MCADFetchDelayStage::execute(llvm::mca::InstRef &IR) {
+    notifyEvent<llvm::mca::HWInstructionEvent>(llvm::mca::HWInstructionEvent(llvm::mca::HWInstructionEvent::LastGenericEventType, IR));
+    const llvm::mca::Instruction *I = IR.getInstruction();
+    const llvm::mca::InstrDesc &ID = I->getDesc();
+    const llvm::MCInstrDesc &MCID = MCII.get(I->getOpcode());
+    bool immediatelyExecute = true;
+    unsigned delayCyclesLeft = 0;
+    if(MCID.isBranch()) {
+        // delayed, will have to wait
+        delayCyclesLeft = 100;
+    }
+    instrQueue.emplace_back(DelayedInstr { delayCyclesLeft, IR });
+    // if the instruction is not delayed, execute it immediately (it will
+    // have a delayCyclesLeft of 0 and be at the top of the queue)
+    return forwardDueInstrs();
+}
+
+llvm::Error MCADFetchDelayStage::cycleStart() {
+    if(!instrQueue.empty()) {
+        instrQueue.front().delayCyclesLeft--;
+    }
+    return forwardDueInstrs();
+}
+
+}
+}
